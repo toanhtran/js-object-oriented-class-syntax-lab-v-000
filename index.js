502747f9532c121318cf781298c8c509cf76b347
ToAnh Tran <toanh.t.tran@gmail.com>
2018-11-07 00:57:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ class BoardMember {
   }
 
   sayHi() {
-    return `Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}.`
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
 }
 
