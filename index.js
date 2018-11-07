0aebb26f872e26d0522c75cfe78e91649e2867fb
ToAnh Tran <toanh.t.tran@gmail.com>
2018-11-07 00:48:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 class BoardMember {
+  constructor(name, homeState, training) {
   this.name = name;
   this.homeState = homeState;
   this.training = training;
 }
 
-veto() {
-  console.log(`Hello, my name is ${this.name}`);
+  veto() {
+    console.log(`Hello, my name is ${this.name}`);
+  }
 }
