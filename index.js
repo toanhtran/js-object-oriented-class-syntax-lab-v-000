803c9055da3f4bb33ea43284fd624d2a0a8bddf7
ToAnh Tran <toanh.t.tran@gmail.com>
2018-11-07 00:53:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ class BoardMember {
   releasePressStatement() {
     return "You will see great things from Scuber.";
   }
+
+  sayHi() {
+    return `Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}.`
+  }
 }
 
 class Ceo extends BoardMember {
